2623b78a2db229b280b36fcc89695d058e6326fb
Danil Treshchev <Madaspeee@yandex.ru>
2022-03-24 22:19:58 +0500

    add loguru logger

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import requests
 import time
+from loguru import logger
 
 from type import Event
 
 
 class Bot:
-    def __init__(self, token): 
+    def __init__(self, token):
         self.__token = token 
         self.__telegram_api_url = "https://api.telegram.org/bot{token}/{method}".format(
             token=self.__token, method="{method}"
         )  
 
+        logger.info("Верификация токена")
         self.__verify_token()
+        logger.info("Верефикация токена успешна")
 
     def get_me(self):
         method_url = self.__telegram_api_url.format(method="getMe")
@@ -27,9 +30,11 @@ class Bot:
     def __verify_token(self):
         token_status = self.get_me()["ok"]
         if not token_status:
+            logger.error("Верефикация токена не успешна")
             raise Exception("Токен бота не верен")
 
     def start_pooling(self):
+        logger.info("Бот запускается")
         method_url = self.__telegram_api_url.format(
             method="getUpdates"
         )  
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from bot import Bot
 from config import BOT_TOKEN
+from loguru import logger
+
+logger.add("my_log.log", colorize=True, format="<green>{time}</green> <level>{message}</level>")
 
 my_bot = Bot(token=BOT_TOKEN)
 
 for event in my_bot.start_pooling():
-    print(event.message.chat.username)
+    logger.info("Новый ивент")
